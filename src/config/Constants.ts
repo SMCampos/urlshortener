7d57f1fe8c367ded99033d9b87992315413101e0
SMCampos <sandrocampos72@gmail.com>
2021-10-09 17:59:42 -0300

    Alterado link conexão DB

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
     API_URL: 'http://localhost:5000',
-    MONGO_CONNECTION: 'mongodb+srv://sandrocampos:AFPG7QUbyGkPfJsi@urlshortner.eidls.mongodb.net/urlshortner?retryWrites=true&w=majority',
+    MONGO_CONNECTION: 'mongodb+srv://sandrocampos:<senha>@urlshortner.eidls.mongodb.net/urlshortner?retryWrites=true&w=majority',
 }
